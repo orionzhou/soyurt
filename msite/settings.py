cf08e575dc3cc3668c460cda5b806719eef5597d
orionzhou <zhoupenggeni@gmail.com>
2016-03-20 12:29:13 -0500

    mysqldbname

--- msite/settings.py
+++ msite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'orionzhou@soyurt',
+        'NAME': 'orionzhou$soyurt',
         'USER': 'orionzhou',
         'PASSWORD': 'mysql-pw',
         'HOST': 'orionzhou.mysql.pythonanywhere-services.com',
